03800dd3f4b6ec5d8123f6a7ed8a00cdbb279549
Ramit <ramitsarkar902@gmail.com>
2021-09-22 11:52:09 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./app.css";
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+
+        <Home />
+      </div>
+    </div>
+  );
 }
 
 export default App;
