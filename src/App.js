0621dcf430b408ca6dbedbac8f4a2d1a4e524a3c
Ramit <ramitsarkar902@gmail.com>
2021-09-27 16:38:13 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import "./app.css";
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
-import Home from "./pages/Home";
+import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
 
 function App() {
   return (
-    <div className="App">
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-
-        <Home />
+    <Router>
+      <div className="App">
+        <Topbar />
+        <div className="container">
+          <Sidebar />
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/users">
+              <UserList />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
