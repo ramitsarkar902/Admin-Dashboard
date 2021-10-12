b71bf0a7a434b113d21401947e8a8debb3842708
Ramit <ramitsarkar902@gmail.com>
2021-10-12 11:46:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
 
 function App() {
   return (
@@ -18,6 +20,12 @@ function App() {
             </Route>
             <Route path="/users">
               <UserList />
+            </Route>
+            <Route path="/user/:userId">
+              <User />
+            </Route>
+            <Route path="/newUser">
+              <NewUser />
             </Route>
           </Switch>
         </div>
