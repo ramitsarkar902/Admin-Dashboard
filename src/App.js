0ddc57060538b54d96beb630fe4a889638507dbc
Ramit <ramitsarkar902@gmail.com>
2021-10-13 11:11:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
 
 function App() {
   return (
@@ -34,6 +35,9 @@ function App() {
             </Route>
             <Route path="/product/:productId">
               <Product />
+            </Route>
+            <Route path="/newProduct">
+              <NewProduct />
             </Route>
           </Switch>
         </div>
